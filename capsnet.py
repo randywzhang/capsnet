f400560454ebb43f976392c0f25b030f48768452
Randy Zhang <randywzhang@gmail.com>
2023-01-23 12:19:02 -0700

    weight initializer, documentation

--- capsnet.py
+++ capsnet.py
@@ -14,7 +14,7 @@ implementation works well and that dynamic routing helps.'
 """
 
 import keras.backend as K
-from keras import layers
+from keras import initializers, layers
 import tensorflow as tf
 
 """
@@ -34,6 +34,7 @@ class CapsuleLayer(layers.Layer):
     build method for custom Keras layer
     """
     def build(self, input_shape):
+        # TODO: figure out how to attach to non-capsule layer
         # build must support linking to both capsule and non capsule layers
         # if input_shape is 2-D (vector.shape = (1, x)) we need to add a 3rd dimension
         #
@@ -42,16 +43,43 @@ class CapsuleLayer(layers.Layer):
         # will give us the activations of the current layer capsules of shape
         # (1, caps_dims, num_caps)
         # TODO: determine the shape of the weight matrix that allows for easy multiplication
+        # scratch notes:
+        # for each connection between an input capsule xi with vector shape [id, 1]
+        # and an output capsule yj with shape [od, 1] we need a weight matrix Wij
+        # with shape [od, id] so that Wij * xj -> shape [od, 1]
+        # There will be a matrix ([on, in]) of matrices ([od, id])
+        # input looks like [in, id, 1] output looks like [on, od, 1]
+        # weights look like [on, in, od, id]
+        # W * i -> [on, in, od, 1]
+        # reduce sum along in axis (axis 1) results in [on, od, 1]      **Note: We don't want to reduce sum
+        #                                                                       we want to apply routing
+        # 'each capsule in the [6 × 6] grid is sharing their
+        # weights with each other' (p.4, Hinton et. al)
+        # Each capsule belonging to the the same feature detector
+        # shares the same weights.
+        # TODO: split input_capsule_dimensions to reflect this
+
+        self.input_num_capsules = input_shape[1]
+        self.input_capsule_dimension = input_shape[2]
+
         self.weights = self.add_weight(name='weights',
-                                       shape=(input_shape[1], input_shape[2],
-                                              self.num_capsules, self.capsule_dimension))
+                                       shape=(self.num_capsules, self.input_num_capsules,
+                                              self.capsule_dimension, self.input_capsule_dimension),
+                                       initializer=weight_initializer,
+                                       trainable=True)
+
         super(CapsuleLayer, self).build(input_shape)
 
     """
     call method for custom Keras layer
+    
+    @param input_data - input to the capsule layer. If the input is
+        from another capsule layer, it will have shape (num_caps, caps_dims, 1)
     """
     def call(self, input_data):
         # TODO: implement call method for custom Keras layer
+        # First multiply weight matrix and inputs
+        u_hat = tf.matmul(self.weights, input_data)
         pass
 
     """
@@ -82,3 +110,13 @@ def squash(s):
     # v = |s|^2 / (1 + |s|^2) * s / |s|            (Eqn. 1)
     # the addition of K.epsilon is to prevent division by 0
     return s * squared_norms / (1 + squared_norms) / (tf.sqrt(squared_norms) + K.epsilon())
+
+
+"""
+Custom weight initializer
+
+https://keras.io/api/layers/initializers/#creating-custom-initializers
+"""
+def weight_initializer(shape, dtype=None):
+    initializer = initializers.initializers_v1.RandomNormal
+    return initializer(shape, dtype)
