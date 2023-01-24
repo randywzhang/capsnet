f539a6dc73b5cf1b411f23640df9c135f8620941
Randy Zhang <randywzhang@gmail.com>
2023-01-24 08:46:22 -0700

    test boilerplate

---
+++ capsnet_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestCapsNet(unittest.TestCase):
+
+    def setup(self):
+        pass
+
+    def test_build(self):
+        pass
+
+    def test_call(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
